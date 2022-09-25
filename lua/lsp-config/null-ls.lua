0c4db809c45c0e47d3691261c38311ec665a4079
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-09-25 20:58:49 +0300

    Add flake8

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -1,20 +1,22 @@
-	-- formatting.latexindent,
+-- formatting.latexindent,
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
-	formatting.autopep8,
-	formatting.stylua,
-    formatting.markdownlint
+    formatting.autopep8,
+    formatting.stylua,
+    formatting.markdownlint,
+    diagnostics.flake8
 }
 
 null_ls.setup({
-	sources = sources,
+    sources = sources,
 
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
-		end
-	end,
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+        end
+    end,
 })
